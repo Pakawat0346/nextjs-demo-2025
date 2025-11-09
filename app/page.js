d0b580ca18e70b1945b5227d85dbb80e7368d4c1
ttt <pakawat.chav@ku.th>
2025-11-09 16:11:08 +0700

    Product

--- app/page.js
+++ app/page.js
@@ -1,17 +1,67 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import FormComponent from "@/components/FormComponent";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+  const [filteredProduct, setFilteredProduct] = useState([]); 
+  const router = useRouter();
+
+  const getAllProduct = async () => {
+    try{
+    const response = await fetch("https://dummyjson.com/product");
+
+    if (!response.ok) throw new Error("Failed");
+
+    const allProduct = await response.json();
+    setProducts(allProduct.products);
+    setFilteredProduct(allProduct.products);
+  } catch (error) {
+    setProducts([]);
+    setFilteredProduct([]);
+  }
+};
+
+console.log('-products-', products);
+console.log('filteredProduct', filteredProduct);
+
+
+useEffect(() => {
+  getAllProduct();
+},[]);
+
+const handleSearch = (text) => {
+  const filterProduct= products.filter((p)=> 
+    p.title.toLowerCase().includes(text.toLowerCase()));
+
+  setFilteredProduct(filterProduct)
+}
+
   return (
     <div>
       <Header />
-      <FormComponent />
+      <FormComponent onSearch={handleSearch} />
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 m-10">
-        {/* Code */}
+        {filteredProduct.map((item) => (
+          <div 
+            key={item.id}
+            className="flex flex-col items-center border border-gray-300 rounded-3xl hover:shadow-lg curser-pointer"
+            onClick={() => router.push(`/products/${item.id}`) }
+          >
+            <img
+              alt={item.title}
+              src={item.thumbnail}
+            />
+
+            <p className="text-[16px] font-bolt ">{item.title}</p>
+            <p className="text-[16px]">{item.price}</p>
+
+          </div>
+        ))}
       </div>
     </div>
   );
